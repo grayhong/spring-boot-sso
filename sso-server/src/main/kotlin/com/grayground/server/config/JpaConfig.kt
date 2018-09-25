96fefbbced4a7569b3e215c2daf3def051fe8b16
Youngkyu <yk.hong@kaist.ac.kr>
2018-09-26 01:36:20 +0900

    JpaConfig setting for scanning entity and repository

--- sso-server/src/main/kotlin/com/grayground/server/config/JpaConfig.kt
+++ sso-server/src/main/kotlin/com/grayground/server/config/JpaConfig.kt
@@ -1,4 +1,16 @@
 package com.grayground.server.config
 
+import com.grayground.server.entity.EmailUser
+import com.grayground.server.repository.UserRepository
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.transaction.annotation.EnableTransactionManagement
+
+@EntityScan(basePackageClasses = [EmailUser::class])
+@EnableJpaRepositories(basePackageClasses = [UserRepository::class])
+@EnableTransactionManagement(order = 20)
+@Configuration
 class JpaConfig {
+
 }
